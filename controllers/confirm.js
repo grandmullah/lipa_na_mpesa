cabede7d46bfbfc71fdca1aeaf52538080c40ad4
grandmullah <collinsbiegon9@gmail.com>
2022-02-25 14:48:52 +0300

    fixed

--- controllers/confirm.js
+++ controllers/confirm.js
@@ -19,7 +19,7 @@ async function confirmation(data) {
     console.log(typeof ge.TxHash)
     
 
-    if (data.ResultCode === 0 && typeof ge.TxHash !== 'undefined'){
+    if (data.ResultCode === 0 && typeof ge.TxHash === 'undefined'){
         console.log("here",data.CallbackMetadata.Item[0].Value)
         const amount =  ethers.utils.parseEther(`${data.CallbackMetadata.Item[0].Value}`)
         let tx = await  usdContract.mint(addr,amount)
